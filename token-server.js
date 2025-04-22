0200e6ea258441737feac6078cecddbf6bc688b4
AliZeynalovv <alizeynalov345@gmail.com>
2025-04-22 14:28:23 +0400

    Create  token-server.js

---
+++ token-server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/tokenal", (req, res) => {
+    const token = generateFakeToken();
+    res.json({ token });
+});
+
+function generateFakeToken() {
+    return Math.random().toString(36).substr(2) + Date.now().toString(36);
+}
+
+app.listen(PORT, () => {
+    console.log(`Token sunucusu çalışıyor http://localhost:${PORT}`);
+});
